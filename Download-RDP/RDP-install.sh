be9ba9d351fa0886d866abce8ccbe9b05db2d25a
NejcKete <160256973+NejcKete@users.noreply.github.com>
2024-06-16 18:02:24 +0200

    Download

---
+++ Download-RDP/RDP-install.sh
@@ -0,0 +1,16 @@
+sudo apt install remmina-plugin-rdp -y
+sudo apt install openjdk-17-jdk -y
+sudo apt-get install openssh-client -y
+sudo mkdir -p ~/.local/share/remmina
+sudo apt remove galculator -y
+sudo apt remove chromium-* -y
+sudo apt remove pcmanfm -y
+mv USERS.txt ~
+mv RDP.jar ~
+mv RDP.sh ~/Desktop
+chmod 777 ~/Desktop/RDP.sh
+mv test.remmina ~/.local/share/remmina/
+mv rdp.desktop ~/.config/autostart/rdp.desktop
+mv lxde-rc.xml ~/.config/openbox/
+sed -i '/@lxpanel --profile LXDE/s/^/#/' /etc/xdg/lxsession/LXDE/autostart
+sudo reboot
